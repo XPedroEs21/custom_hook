1689c6ec67baad7a7e851bbe6b182d912d3aa45c
Pedro <xpedroes@gmail.com>
2024-07-08 12:19:42 -0600

    Added the Fetch functions

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UseFetch from './UseFetch'
+import './FetchData.css'
 
 const FetchData = () => {
+  const [data]=UseFetch('https://api.npoint.io/9045c260b1565daa9e15');        //Implement the custom hook by passing the specific url from which you want to fetch data inside the FetchData arrow function
+  console.log(data);                                                          //console.log to check whether data has been retrieved in the variable data
   return (
     <>
      <ul className='list_data_main'>
-        <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
+        <h1 className='usefetch_heading'>Fruits Guide and Benefits</h1>
+        {data && data.map((e)=>(                                              //retrieve the data
+          <>
+          <li className='list_data'>                                          {/*Deploys list of the retrieved data and its atributes*/}
+            <h3>{e.name}</h3>
+            <p><strong>Importance:</strong>{e.importance}</p>
+            <p><strong>Benefits:</strong>{e.benefits}</p>
+            <p><strong>Time: </strong>{e.best_time_to_intake}</p>
+          </li>
+          </>
+        ))}
      </ul>
     </>
   )
--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,12 @@
+import React, { useEffect, useState } from 'react';
+
 const UseFetch = (url) => {
-    
+    const [data, setData]=useState();                               //variable named data using useState hook along with setData as the function
+    useEffect(()=>{                                                 //This is a React Hook used for performing side effects in functional components. It’s typically used for fetching data, subscribing to events, or any other side effects that don’t involve rendering.
+        fetch(url).then((res)=>res.json())                          //This initiates an HTTP request to the specified url, .then(res) converts the response from the server to JSON
+        .then((data)=>setData(data))                                //This sets the retrieved data to the state variable data. setData is a function that updates the state in React functional components.
+    },[])
+    return [data]
 }
 
 export default UseFetch
